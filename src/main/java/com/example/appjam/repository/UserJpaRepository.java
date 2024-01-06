f2472b05de0415d30433c6f49e616d75f37d94cb
신희성 <vvwv3092@gmail.com>
2024-01-06 17:04:11 +0900

    Add login service

--- src/main/java/com/example/appjam/repository/UserJpaRepository.java
+++ src/main/java/com/example/appjam/repository/UserJpaRepository.java
@@ -4,4 +4,5 @@ import com.example.appjam.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByToken(String token);
 }
--- src/main/java/com/example/appjam/service/UserService.java
+++ src/main/java/com/example/appjam/service/UserService.java
@@ -1,13 +1,26 @@
 package com.example.appjam.service;
 
 import com.example.appjam.controller.dto.LoginRequest;
+import com.example.appjam.domain.UserEntity;
+import com.example.appjam.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    public void login(LoginRequest request) {
 
+    private final UserJpaRepository userJpaRepository;
+
+    @Transactional
+    public void login(LoginRequest request) {
+        if (!userJpaRepository.existsByToken(request.getToken())) {
+            UserEntity user = UserEntity.builder()
+                    .userName(request.getUserName())
+                    .token(request.getToken())
+                    .build();
+            userJpaRepository.save(user);
+        }
     }
 }
