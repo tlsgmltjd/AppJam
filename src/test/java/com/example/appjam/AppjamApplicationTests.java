ec038bcb7fe15f4a651e5992058a9e4ab40e1d73
신희성 <vvwv3092@gmail.com>
2024-01-06 15:19:42 +0900

    init

---
+++ src/test/java/com/example/appjam/AppjamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
