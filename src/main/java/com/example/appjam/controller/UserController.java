5abbffde675e25515817509b74da45cac234c2db
신희성 <vvwv3092@gmail.com>
2024-01-06 17:05:44 +0900

    Add login controller

--- src/main/java/com/example/appjam/controller/UserController.java
+++ src/main/java/com/example/appjam/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.appjam.controller;
 
 import com.example.appjam.controller.dto.LoginRequest;
+import com.example.appjam.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping("/login")
     public ResponseEntity<Void> login(LoginRequest request) {
-
+        userService.login(request);
+        return ResponseEntity.noContent().build();
     }
+
 
 }
